f57a887c77e8534d3d2c5ef878723efc8c86425d
Balaji Pothuganti <125890969+Balajinanda@users.noreply.github.com>
2023-05-11 23:15:58 +0100

    forecast defining

--- 22023213 Balaji.py
+++ 22023213 Balaji.py
@@ -29,3 +29,41 @@ def logistic(t, n0, g, t0):
     """
     f = n0 / (1 + np.exp(-g*(t - t0)))
     return f
+
+def plot_forecast(year, data, param, covar, predictions=None):
+    """
+    Plots the forecasted CO2 emissions along with the confidence \
+        interval and predictions.
+
+    Parameters:
+        year (array-like): Years for the forecast.
+        data (DataFrame): Data containing 'Year' and 'CO2 emissions' columns.
+        param (array-like): Parameters for the logistic function.
+        covar (array-like): Covariance matrix from curve_fit.
+        predictions (array-like, optional): Predicted CO2 emissions values. \
+            Default is None.
+
+    Returns:
+        None
+    """
+    forecast = logistic(year, *param)
+
+    # Calculate confidence interval
+    stderr = np.sqrt(np.diag(covar))
+    conf_interval = 1.96 * stderr
+    upper = logistic(year, *(param + conf_interval))
+    lower = logistic(year, *(param - conf_interval))
+
+    # Plot the result
+    plt.figure()
+    plt.plot(data["Year"], data["CO2 emissions"], label="CO2 emissions")
+    plt.plot(year, forecast, label="Forecast")
+    plt.fill_between(year, upper, lower, color='purple', alpha=0.2, \
+                     label="95% Confidence Interval")
+    if predictions is not None:
+        plt.plot(year_pred, predictions, 'ro-', label='Predictions')
+    plt.xlabel("Year")
+    plt.ylabel("CO2 emissions")
+    plt.title("Logistic forecast for China")
+    plt.legend()
+    plt.show()
